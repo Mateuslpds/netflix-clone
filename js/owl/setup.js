fc9d8fc540dbd1face79c7d97561ea4d5c4d9c3b
Mateuslpds <mateusyasmim2014@gmail.com>
2021-05-25 16:44:20 -0300

    Primeiro commit

---
+++ js/owl/setup.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+	center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+    	0:{
+    		items: 4
+    	},
+        780:{
+            items: 6
+        }
+    }
+})
